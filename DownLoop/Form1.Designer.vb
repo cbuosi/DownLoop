273aedbcf96ebc85819764f5f41606d47b1543e9
Carlos Buosi <cbuosi@gmail.com>
2020-05-16 19:18:12 -0300

    Adicionar arquivos de projeto.

---
+++ DownLoop/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.oWB = New System.Windows.Forms.WebBrowser()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'oWB
+        '
+        Me.oWB.Location = New System.Drawing.Point(0, 0)
+        Me.oWB.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.oWB.Name = "oWB"
+        Me.oWB.Size = New System.Drawing.Size(656, 396)
+        Me.oWB.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(357, 415)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Google...."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(447, 415)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Zip"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(528, 415)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "PDF"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.oWB)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents oWB As WebBrowser
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ DownLoop/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Net
+
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        oWB.Navigate("www.google.com")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        oWB.Navigate("www.simulaimport.com.br/Teste/Arquivo.zip")
+    End Sub
+
+    Private Sub oWB_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles oWB.Navigating
+
+        If e.Url.Segments(e.Url.Segments.Length - 1).EndsWith(".zip") Then
+            e.Cancel = True
+            Dim arquivo1 As String = ""
+            Dim oWC As New WebClient
+            oWC.DownloadFile(e.Url, "c:\temp\arq.zip")
+        End If
+
+        If e.Url.Segments(e.Url.Segments.Length - 1).EndsWith(".pdf") Then
+            e.Cancel = True
+            Dim arquivo1 As String = ""
+            Dim oWC As New WebClient
+            oWC.DownloadFile(e.Url, "c:\temp\arq.pdf")
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        oWB.Navigate("www.simulaimport.com.br/Teste/Arquivo.pdf")
+
+    End Sub
+End Class
